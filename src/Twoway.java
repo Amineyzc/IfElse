9170b71cf06d74dad06e6791573a7517d6b162b3
Amine Ileri <yaziciamine@gmail.com>
2022-06-06 18:56:34 -0400

    "new commit"

--- src/Twoway.java
+++ src/Twoway.java
@@ -21,6 +21,7 @@ public class Twoway {
             if (num % 5 == 0) {
                 System.out.println("Number is divisible by 5");
             } else {System.out.println("Number is not divisible by 5");
+                //changes are here
         }
 
 
